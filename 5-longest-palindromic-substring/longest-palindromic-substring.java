f236b7ca71c5524c0f42e5c39991b271b94aeef6
Irfan Sayyad <irfansayyad3505@gmail.com>
2026-08-01 11:38:17 +0530

    Time: 14 ms (89.80%) | Memory: 43.6 MB (69.41%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,27 +1,55 @@
 class Solution {
+    int start =0;
+    int maxlen=0;
     public String longestPalindrome(String s) {
-        int n= s.length();
-        String ans="";
-        for(int i=0; i<n;i++){
-            for(int j=i; j<n ;j++){
-                if(isPalindrome(s,i,j)){
-                    if((j-i+1) > ans.length()){
-                        ans = s.substring(i,j+1);
-                    }
-                }
+        for(int i=0; i<s.length(); i++){
+            expand(s,i,i);
+            expand(s,i,i+1);
+        }
+        return s.substring(start, start+maxlen);
+    }
+    private void expand(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            if(right-left+1 > maxlen){
+                maxlen = right-left+1;
+                start = left;
             }
+            left--;
+            right++;
         }
-        return ans;
     }
+}
 
-    private boolean isPalindrome(String s, int left, int right){
-        while(left < right){
-        if(s.charAt(left) != s.charAt(right)){
-            return false;
-        }
-        left++;
-        right--;
-    }
-    return true;
-}
-}
+
+
+
+
+
+
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         int n= s.length();
+//         String ans="";
+//         for(int i=0; i<n;i++){
+//             for(int j=i; j<n ;j++){
+//                 if(isPalindrome(s,i,j)){
+//                     if((j-i+1) > ans.length()){
+//                         ans = s.substring(i,j+1);
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+
+//     private boolean isPalindrome(String s, int left, int right){
+//         while(left < right){
+//         if(s.charAt(left) != s.charAt(right)){
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
+// }
