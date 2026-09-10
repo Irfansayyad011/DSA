4619c1f9aeb3f75388e859d9642e0757282b8c61
Irfan Sayyad <irfansayyad3505@gmail.com>
2026-09-10 09:57:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (71.83%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,13 +1,20 @@
 class Solution {
     public char repeatedCharacter(String s) {
         Set<Character>set = new HashSet<>();
-        for(int i=0; i<s.length(); i++){
-            if(set.contains(s.charAt(i))){
-                return s.charAt(i);
+        for(char c : s.toCharArray()){
+            if(set.contains(c)){
+                return c;
             }else{
-                set.add(s.charAt(i));
+                set.add(c);
             }
-        }    
-        return 'z';
+        }
+        // for(int i=0; i<s.length(); i++){
+        //     if(set.contains(s.charAt(i))){
+        //         return s.charAt(i);
+        //     }else{
+        //         set.add(s.charAt(i));
+        //     }
+        // }    
+        return ' ';
         }
 }
